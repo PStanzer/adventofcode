eb3158d26d91e45653d245768741fc5962d731ac
PStanzer <152657138+PStanzer@users.noreply.github.com>
2023-12-08 14:41:13 +0100

    Update day8-2.py

--- 2023/day8-2.py
+++ 2023/day8-2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 file1 = open('input.txt', 'r')
 Lines = file1.read().split("\n")
 
@@ -6,29 +8,23 @@ directions=[make_binary(x) for x in Lines[0]]
 nodes={x.split('=')[0].strip():x.split('=')[1].strip().strip('()').split(', ') for x in Lines[2:]}
 
 check_start = lambda x: x if x[2]=='A' else None
-start=[check_start(x.split('=')[0].strip()) for x in Lines[2:] if check_start(x.split('=')[0].strip()) is not None]
-print(start)
+current=[check_start(x.split('=')[0].strip()) for x in Lines[2:] if check_start(x.split('=')[0].strip()) is not None]
+
 check_finish = lambda x: True if x[2]=='Z' else False
 
 i=0
-res=0
-#print(['A' for x in range(len(start))])
-#print(check_node(start[0],'A'))
-current=start
+cnt=0
+res=[0,0,0,0,0,0]
 while 1:
-#    print(i)
     if i >= len(directions):
-#        break
         i=0
     current=[nodes[node][directions[i]] for node in current]
-    print(current)
     i+=1
-    res+=1
-    print(res)
-    if False in list(map(check_finish,current)):
-        continue
-    else:
+    cnt+=1
+    test=list(map(check_finish,current))
+    if True in test:
+        res[test.index(True)]=cnt
+    if 0 not in res:
         break
 
-print('res:',res)
-    
+print(np.lcm.reduce(res))
